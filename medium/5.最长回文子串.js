d2577c5d1fe0b0a8b37789fe996d259f156dff35
yugef3h <245868066@qq.com>
2019-02-09 12:42:55 +0800

    challenge continue

--- medium/5.最长回文子串.js
+++ medium/5.最长回文子串.js
@@ -1,9 +1,11 @@
 /**
 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000
+
 输入: "babad"
 输出: "bab"
 注意: "aba" 也是一个有效答案。
- */
+*/
+
 // ?
 var longestPalindrome = function (s) {
   if (!s || s.length < 2) {
